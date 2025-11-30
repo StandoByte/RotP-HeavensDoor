dfac4a0472bf1f9d9d4bd7909c53ce380559da3e
StandoByte <94539965+StandoByte@users.noreply.github.com>
2025-11-30 22:18:48 +0300

    removed redundant code copypaste with 1st person hand rendering

--- src/main/java/com/yaricktt/heavensdoor/client/render/entity/layerrenderer/BookLayer.java
+++ src/main/java/com/yaricktt/heavensdoor/client/render/entity/layerrenderer/BookLayer.java
@@ -1,6 +1,7 @@
 package com.yaricktt.heavensdoor.client.render.entity.layerrenderer;
 
 import com.github.standobyte.jojo.client.ClientUtil;
+import com.github.standobyte.jojo.client.render.entity.layerrenderer.IFirstPersonHandLayer;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import com.yaricktt.heavensdoor.RotpHDAddon;
@@ -9,7 +10,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.IEntityRenderer;
 import net.minecraft.client.renderer.entity.LivingRenderer;
 import net.minecraft.client.renderer.entity.PlayerRenderer;
@@ -24,7 +24,6 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.HandSide;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Util;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 
 import javax.annotation.Nullable;
@@ -130,21 +129,7 @@ public class BookLayer<T extends LivingEntity, M extends EntityModel<T>> extends
         }
     }
 
-    public static void renderFirstPerson(HandSide side, MatrixStack matrixStack,
-                                         IRenderTypeBuffer buffer, int light, AbstractClientPlayerEntity player) {
-        EntityRenderer<?> renderer = Minecraft.getInstance().getEntityRenderDispatcher().getRenderer(player);
-        if (renderer instanceof PlayerRenderer) {
-            PlayerRenderer playerRenderer = (PlayerRenderer) renderer;
-            if (RENDERER_LAYERS.containsKey(playerRenderer)) {
-                BookLayer<?, ?> layer = RENDERER_LAYERS.get(playerRenderer);
-                if (layer != null) {
-                    layer.renderHandFirstPerson(side, matrixStack,
-                            buffer, light, player, playerRenderer);
-                }
-            }
-        }
-    }
-
+    @Override
     public void renderHandFirstPerson(HandSide side, MatrixStack matrixStack,
                                        IRenderTypeBuffer buffer, int light, AbstractClientPlayerEntity player,
                                        PlayerRenderer playerRenderer) {
